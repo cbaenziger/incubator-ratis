da5fcda89688c2c15b5fec57169503fc0742d8c1
Clay Baenziger <cbaenziger@bloomberg.net>
2019-10-08 15:53:45 -0400

    Try Yetus 0.11.0

--- dev-support/ci/common.sh
+++ dev-support/ci/common.sh
@@ -32,7 +32,7 @@ mkdir -p "${ARTIFACTS}" "${YETUSDIR}" "${TOOLS}"
 cp $BASEDIR/dev-support/yetus-personality.sh $WORKSPACE/
 cp $BASEDIR/dev-support/docker/Dockerfile $WORKSPACE/
 
-YETUS_VERSION=${YETUS_VERSION:-0.8.0}
+YETUS_VERSION=${YETUS_VERSION:-0.11.0}
 echo "Downloading Yetus"
-curl -L https://archive.apache.org/dist/yetus/${YETUS_VERSION}/yetus-${YETUS_VERSION}-bin.tar.gz -o yetus.tar.gz
+curl -L https://archive.apache.org/dist/yetus/${YETUS_VERSION}/apache-yetus-${YETUS_VERSION}-bin.tar.gz -o yetus.tar.gz
 gunzip -c yetus.tar.gz | tar xpf - -C "${YETUSDIR}" --strip-components 1
