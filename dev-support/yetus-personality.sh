ab9a531f7d576741b373cfc9852d85d55b9ca2de
Clay Baenziger <cbaenziger@bloomberg.net>
2019-10-11 18:11:03 -0400

    Try export

--- dev-support/yetus-personality.sh
+++ dev-support/yetus-personality.sh
@@ -36,8 +36,8 @@ function personality_globals
 
 function ratis_docker_support
 {
-  DOCKER_EXTRAARGS+=( $(source dev-support/binary_locations.sh && env|awk '/^ratis_.*/{printf "--build-arg \"" $1 "\" "}') )
-  #export DOCKER_EXTRAENVS+=( $(source sourcedir/dev-support/binary_locations.sh && env|awk 'BEGIN{FS="="};/^ratis_.*/{printf "--build-arg " $1 " "}') )
+  export DOCKER_EXTRAARGS+=( $(source dev-support/binary_locations.sh && env|awk '/^ratis_.*/{printf "--build-arg \"" $1 "\" "}') )
+##  export DOCKER_EXTRAENVS+=( $(source dev-support/binary_locations.sh && env|awk '/^ratis_.*/{printf $1}') )
   yetus_debug "Using DOCKER_EXTRAARGS: ${DOCKER_EXTRAARGS[*]}"
 }
 
