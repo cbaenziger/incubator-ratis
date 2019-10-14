c06b64bb650f127645dc2386da39fdafaa2c3dbf
Clay Baenziger <cbaenziger@bloomberg.net>
2019-10-14 00:15:14 -0400

    Try not quoting build-args from awk

--- dev-support/yetus-personality.sh
+++ dev-support/yetus-personality.sh
@@ -36,7 +36,7 @@ function personality_globals
 
 function ratis_docker_support
 {
-  export DOCKER_EXTRABUILDARGS+=( $(source dev-support/binary_locations.sh && env|awk '/^ratis_.*/{printf "--build-arg \"" $1 "\" "}') )
+  export DOCKER_EXTRABUILDARGS+=( $(source dev-support/binary_locations.sh && env|awk '/^ratis_.*/{printf "--build-arg " $1 " "}') )
 ##  export DOCKER_EXTRAENVS+=( $(source dev-support/binary_locations.sh && env|awk 'BEGIN{FS="="}; /^ratis_.*/{printf $1 " "}') )
 ##  yetus_debug "Using DOCKER_EXTRAENVS: ${DOCKER_EXTRAENVS[*]}"
 ##  for i in ${DOCKER_EXTRAENVS[*]}; do
